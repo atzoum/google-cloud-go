61b5b592668f0b8a51889f21d53b11774f93bba2
Aris Tzoumas <atzoumas@rudderstack.com>
2024-05-20 15:15:23 +0300

    chore(bigquery): debugging driver hanging

--- bigquery/bigquery.go
+++ bigquery/bigquery.go
@@ -252,7 +252,7 @@ var (
 
 	// These reasons are used exclusively for enqueuing jobs (jobs.insert and jobs.query).
 	// Using them for polling may cause unwanted retries until context deadline/cancellation/etc.
-	jobRetryReasons = []string{"backendError", "rateLimitExceeded", "jobRateLimitExceeded", "internalError"}
+	jobRetryReasons = []string{"backendError", "rateLimitExceeded", "internalError"}
 
 	retry5xxCodes = []int{
 		http.StatusInternalServerError,
